378cfb7aec569b76ac34bca70180c3d09b7a2d36
crusty0530 <crwhite0530@gmail.con>
2026-07-14 12:57:26 -0400

    add dashboard with navigation to player setup

--- mobile/pages/Dashboard.tsx
+++ mobile/pages/Dashboard.tsx
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function Dashboard(){
 
+    type RootStackParamList = {
+        Tabs: undefined
+        PlayerSetup: undefined
+        LiveGame: undefined
+    }
+
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
     return(
-        <View>
-            <Text>Dashboard</Text>
+        <View style={styles.container}>
+            <Button title="Start Game" onPress={() => navigation.navigate('PlayerSetup')}/>
+            <Button title="Log Past Game" onPress={() => {}}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 70
+    },
+});
