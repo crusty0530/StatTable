9820af5d9d673bc91c03c32e8946e35e0e6d40da
crusty0530 <crwhite0530@gmail.con>
2026-07-19 18:07:26 -0400

    add live game screen with life tracker

--- mobile/pages/LiveGame.tsx
+++ mobile/pages/LiveGame.tsx
@@ -1,9 +1,56 @@
-import { View, Text } from "react-native";
+import { useRoute, RouteProp } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { RootStackParamList } from "../types";
 
 export default function LiveGame(){
+
+    const route = useRoute<RouteProp<RootStackParamList, 'LiveGame'>>()
+    const { players, timerEnabled, turnLength } = route.params
+
+    const [ lifeTotals, setLifeTotals ] = useState<Record<string, number>>({})
+    const [ turnCount, setTurnCount ] = useState(0)
+    const [ activePlayer, setActivePlayer ] = useState(players[0])
+    const [ timerRunning, setTimerRunning ] = useState(false)
+    const [ remainingTime, setRemainingTimer ] = useState(0)
+    const [ eliminatedPlayers, setEliminatedPlayers ] = useState<string[]>([])
+
+    useEffect(() => {
+        if (players.length < 2) return
+        
+        const initialTotals = players.reduce((acc, player) => {
+            acc[player.username] = 40
+            return acc
+        }, {} as Record<string, number>)
+        setLifeTotals(initialTotals)
+    }, [])
+
+    const adjustLife = (player: string, amount: number) => {
+        setLifeTotals({
+            ...lifeTotals,
+            [player]: lifeTotals[player] + amount
+        })
+    }
+
     return(
-        <View>
-            <Text>Live Game</Text>
+        <View style={styles.container}>
+            {players.map((player, index) => (
+                <View key={index}>
+                    <Text>{player.username}</Text>
+                    <Text>{lifeTotals[player.username]}</Text>
+                    <Button title="+1" onPress={() => adjustLife(player.username, 1)}/>
+                    <Button title="-1" onPress={() => adjustLife(player.username, -1)}/>
+                </View>
+            ))}
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 70
+    },
+});
